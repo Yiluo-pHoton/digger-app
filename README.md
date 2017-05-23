028019bf2c4976af428b94f2555effb420fea3ac
Yiluo pHoton <yiluo.li.astrophysics@gmail.com>
2017-05-23 11:48:09 +0800

    Paired start and stop

--- README.md
+++ README.md
@@ -17,3 +17,19 @@
 ## 关于 Ruff 上 Home 包的问题
 
 由于 Home 不支持 Host JS 静态文件，所以我把 app.js 和 thumbs.0.6.0.min.js 的后缀改成了 .css,  比较 tricky 的做法.
+
+## Digger反应不稳定的问题 ##
+
+在鼠标按下之后，console中时常会在同一时间点记录下连续的指令，如
+```
+stop diggering
+stop diggering
+start diggering
+```
+```
+start diggering
+start diggering
+stop diggering
+stop diggering
+```
+`start`后并不固定搭配`stop`导致指令持续进行，在每个指令前加`status`防止同一`start`类重复出现即可
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 var digger;
 var Server = require('home').Server;
 var server = new Server();
+var diggeringStatus = 0;
+var moveStatus = 0;
+var turnStatus = 0;
 
 $.ready(function (error) {
     if (error) {
@@ -16,62 +19,96 @@ $.ready(function (error) {
     })
     // 对Ruff板上的 Http服务器的`/start-diggering`访问，命令挖掘机向前行
     server.get('/start-diggering', function () {
-        digger.startDiggering(function(){
-            console.log('start diggering');
-        });
+        if (diggeringStatus == 0){
+            diggeringStatus = 1;
+            digger.startDiggering(function(){
+                console.log('start diggering');
+            });
+        }
     });
 
     server.get('/move-forward', function () {
-        digger.moveForward(function() {
-            console.log('move forward');
-        });
+        if (moveStatus == 0){
+            moveStatus = 1;
+            digger.moveForward(function() {
+                console.log('move forward');
+            });
+        }
     });
     
     // 对Ruff板上的 Http服务器的`/move-back`访问，命令挖掘机向后退
     server.get('/move-back', function () {
-        digger.moveBack(function(){
-            console.log('move Back');
-        });
+        if (moveStatus == 0){
+            moveStatus = 1;
+            digger.moveBack(function(){
+                console.log('move Back');
+            });
+        }
     });
     
     server.get('/move-left', function () {
-        digger.moveLeft(function(){
-            console.log('move left');
-        });
+        if (moveStatus == 0){
+            moveStatus = 1;
+            digger.moveLeft(function(){
+                console.log('move left');
+            });
+        }
     });
     
     server.get('/move-right', function () {
-        digger.moveRight(function(){
-            console.log('move right');
-        });
+        if (moveStatus == 0){
+            moveStatus = 1;
+            digger.moveRight(function(){
+                console.log('move right');
+            });
+        }
     });
+
     server.get('/turn-left', function () {
-        digger.turnLeft(function(){
-           console.log('turn left');
-        });
+        if (turnStatus == 0){
+            turnStatus = 1;
+            digger.turnLeft(function(){
+                console.log('turn left');
+            });
+        }
     });
 
     server.get('/turn-right', function () {
-        digger.turnRight(function(){
-            console.log('turn right');
-        });
+        if (turnStatus == 0){
+            turnStatus = 1;
+            digger.turnRight(function(){
+                console.log('turn right');
+            });
+        }
     });
     
     server.get('/stop-diggering', function () {
-        digger.stopDiggering(function () {
-            console.log('stop diggering');
-        });
+        if (diggeringStatus == 1){
+            diggeringStatus = 0;
+            digger.stopDiggering(function () {
+                console.log('stop diggering');
+            });
+        }
     });
+
     server.get('/stop-moving', function () {
-        digger.stopMoving(function () {
-            console.log('stop moving');
-        });
+        if (moveStatus == 1){
+            moveStatus = 0;
+            digger.stopMoving(function () {
+                console.log('stop moving');
+            });
+        }
     });
+
     server.get('/stop-turn-around', function () {
-        digger.stopTurnAround(function () {
-            console.log('stop turn around');
-        });
+        if (turnStatus == 1){
+            turnStatus = 0;
+            digger.stopTurnAround(function () {
+                console.log('stop turn around');
+            });
+        }
     });
+    
     server.listen(6318);
     $('#led-b').turnOn();
 });
